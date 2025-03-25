a924259bed632400d7bc8cb6ac7b389579f07b10
Fernando Nunez <15152269+Zweih@users.noreply.github.com>
2025-03-25 11:58:42 -0600

    Feature: Optimize Pkginfo memory locality (#130)

--- internal/pkgdata/packageinfo.go
+++ internal/pkgdata/packageinfo.go
@@ -2,15 +2,15 @@ package pkgdata
 
 type PkgInfo struct {
 	Timestamp  int64    `json:"timestamp,omitempty"`
+	Size       int64    `json:"size,omitempty"` // package size in bytes
 	Name       string   `json:"name,omitempty"`
 	Reason     string   `json:"reason,omitempty"`  // "explicit" or "dependency"
-	Size       int64    `json:"size,omitempty"`    // package size in bytes
 	Version    string   `json:"version,omitempty"` // current installed version
+	Arch       string   `json:"arch,omitempty"`
+	License    string   `json:"license,omitempty"`
+	Url        string   `json:"url,omitempty"`
 	Depends    []string `json:"depends,omitempty"`
 	RequiredBy []string `json:"requiredBy,omitempty"`
 	Provides   []string `json:"provides,omitempty"`
 	Conflicts  []string `json:"conflicts,omitempty"`
-	Arch       string   `json:"arch,omitempty"`
-	License    string   `json:"license,omitempty"`
-	Url        string   `json:"url,omitempty"`
 }
